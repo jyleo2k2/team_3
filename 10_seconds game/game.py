d04eb5f1ee58ab77e51c88cd521cdc8dfa101695
haeun <hgkdms0810@gmail.com>
2026-05-20 16:09:33 +0900

    수정 내용

--- 10_seconds game/game.py
+++ 10_seconds game/game.py
@@ -2,9 +2,6 @@ import time
 
 def start_game(scores):
 
-    nickname = input("\n닉네임을 입력하세요 : ")
-
-    print("\n=== 10초 맞추기 게임 ===")
     input("시작하려면 Enter를 누르세요!")
 
     start = time.time()
@@ -21,15 +18,8 @@ def start_game(scores):
     if score < 0:
         score = 0
 
-    print("\n당신의 기록 :", round(result, 2), "초")
-    print("10초와의 차이 :", round(difference, 2), "초")
-    print("점수 :", score, "점")
+    scores.append(score)
 
-    scores.append([nickname, score])
+    print(score)
 
-    if difference <= 0.5:
-        print("대박! 거의 정확합니다!")
-    elif difference <= 1:
-        print("잘했어요!")
-    else:
-        print("아쉬워요! 다시 도전!")
+    return score
