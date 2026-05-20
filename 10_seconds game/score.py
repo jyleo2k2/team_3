0fa9935336630c788f45db9e5b1510e1b9a26532
haeun <hgkdms0810@gmail.com>
2026-05-20 15:41:16 +0900

    수정 내용

--- 10_seconds game/score.py
+++ 10_seconds game/score.py
@@ -1,17 +1,23 @@
 def show_score(scores):
 
     if len(scores) == 0:
-        print("\n기록이 없습니다.")
+        return "\n기록이 없습니다."
 
     else:
-        print("\n=== 점수 랭킹 ===")
+
+        result = "\n=== 점수 랭킹 ===\n"
 
         # 점수 높은 순
         ranking = sorted(scores, key=lambda x: x[1], reverse=True)
 
         for i in range(len(ranking)):
-            print(
-                i + 1, "등 :",
-                ranking[i][0],
-                "| 점수 :", ranking[i][1], "점"
+
+            result += (
+                str(i + 1) + "등 : "
+                + ranking[i][0]
+                + " | 점수 : "
+                + str(ranking[i][1])
+                + "점\n"
             )
+
+        return result
